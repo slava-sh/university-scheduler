d3e8d5fb3403d74ef57bc53fcd602ebc43917dc2
Slava Shklyaev <slava@slava.sh>
2017-04-28 13:24:35 +0200

    Add commit history to the submission header

--- generate_submission.sh
+++ generate_submission.sh
@@ -6,12 +6,15 @@ if [[ $(git status --porcelain) ]]; then
 fi
 
 PACKAGE='github.com/slava-sh/university-scheduler'
-COMMIT=$(git rev-parse --short HEAD)
 
 mkdir -p ./out
 {
-  echo "// https://${PACKAGE}"
-  echo "// commit ${COMMIT}"
+  {
+    echo "https://${PACKAGE}"
+    echo
+    git log --oneline --max-count=5
+    echo '...'
+  } | awk '{ print "// " $0 }'
   (cd ./src && bundle -prefix ' ' "${PACKAGE}/src") \
     | tail -n +3 \
     | perl -pe 's/^(\t+local += +)true$/$1false/'
