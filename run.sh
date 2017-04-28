1e6dac6148e2efffefdcf95383b55b9467182706
Slava Shklyaev <slava@slava.sh>
2017-04-28 14:36:35 +0200

    Remove reports
    The current setup fails to measure solution performance and slows development down.

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-if [[ $(git status --porcelain) ]]; then
-  echo 'Please commit all changes'
-  exit 1
-fi
-
-COMMIT=$(git rev-parse --short HEAD)
-
-mkdir -p ./out ./reports
+mkdir -p ./out
 (cd ./src && go build -o ../out/main)
-/usr/bin/time ./out/main | tee "./reports/${COMMIT}.txt"
+/usr/bin/time ./out/main <"$1"
--- src/main.go
+++ src/main.go
@@ -2,55 +2,17 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
 	"time"
 )
 
-const (
-	timeLimit    = 10*time.Second - 60*time.Millisecond
-	local        = true
-	runsPerInput = 5
-)
+const timeLimit = 10*time.Second - 60*time.Millisecond
 
 func main() {
-	if local {
-		runAll()
-	} else {
-		run(os.Stdin, os.Stdout)
-	}
-}
-
-func run(stdin io.Reader, stdout io.Writer) int {
-	in := NewFastReader(stdin)
-	out := bufio.NewWriter(stdout)
+	in := NewFastReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 	problem := ReadProblem(in)
 	solution := Solve(problem, timeLimit)
 	solution.Print(out)
-	return solution.Fatigue
-}
-
-func runAll() {
-	totalScore := 0.0
-	for input := 1; input <= 10; input++ {
-		filename := fmt.Sprintf("input/%02d.txt", input)
-		fmt.Printf("%s ", filename)
-		scoreSum := 0.0
-		for i := 0; i < runsPerInput; i++ {
-			stdin, err := os.Open(filename)
-			if err != nil {
-				panic(err)
-			}
-			score := run(stdin, ioutil.Discard)
-			scoreSum += float64(score)
-			fmt.Printf("%5d ", score)
-		}
-		averageScore := scoreSum / runsPerInput
-		totalScore += averageScore
-		fmt.Printf("%9.3f\n", averageScore)
-	}
-	fmt.Printf("score %9.3f\n", totalScore)
 }
