5acf5403eeeef151e61f1c023ef3f2320acae85d
Slava Shklyaev <slava@slava.sh>
2017-04-30 14:35:31 +0200

    Add the --fast option

--- scheduler/main.cpp
+++ scheduler/main.cpp
@@ -3,10 +3,14 @@
 #include <memory>
 #include "scheduler/time_limited_solver.h"
 
-int main() {
-  const auto time_limit =
-      std::chrono::seconds(10) - std::chrono::milliseconds(50);
+int main(int argc, char** argv) {
   std::ios_base::sync_with_stdio(false);
+
+  auto time_limit = std::chrono::seconds(10) - std::chrono::milliseconds(50);
+  if (argc > 1 && std::string(argv[1]) == "--fast") {
+      time_limit = std::chrono::milliseconds(300);
+  }
+
   auto problem = std::make_shared<scheduler::Problem>();
   std::cin >> *problem;
   auto solution = scheduler::TimeLimitedSolver(time_limit).Solve(problem);
--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@
 go build -o ./out/checker ./checker.go
 for i in $(seq -w 01 10); do
   echo "input/$i.txt --------------------------------------------------"
-  ./out/scheduler <"./input/$i.txt" >"./out/$i.txt"
+  ./out/scheduler --fast <"./input/$i.txt" >"./out/$i.txt"
   cat "./input/$i.txt" "./out/$i.txt" | ./out/checker
 done
